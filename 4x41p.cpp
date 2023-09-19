c48a3e07f8ae094924082088a9e32225df9551b0
Tj Johnston, K4TFJ <timothy.f.johnston@gmail.com>
2023-09-19 14:49:31 -0400

    Create 4x41p.cpp

---
+++ 4x41p.cpp
@@ -0,0 +1,29 @@
+//
+//
+//
+
+int thresholds[16] = {2,77,144,202,244,290,331,368,394,424,452,477,496,518,538,556};
+char keypad[16] = {'1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D'};
+//char keypad[16] = {'1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D'};
+
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int value = analogRead(A0);
+
+  for (int i=0; 1< 16; i++)
+  {
+    if( abs(value - thresholds[i]) < 5)
+    {
+      Serial.println(keypad[i]);
+      whi;e (analogRead(A0) < 1000) {delay(100);}
+    }
+  }
+}
+
+
+
+  
