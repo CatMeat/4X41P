ccc5d7bde9fcf53cb0b7a31d673d08d53ee8f78f
Tj Johnston, K4TFJ <timothy.f.johnston@gmail.com>
2023-09-19 17:59:48 -0400

    Update 4X4P1.ino

--- 4X4P1.ino
+++ 4X4P1.ino
@@ -1,12 +1,9 @@
 //
 //
 //
-char *buffer[20];
-int buflen=0;
-int prevlen=0;
-int thresholds[16] = {2,77,144,202,244,290,331,368,394,424,452,477,496,518,538,556};
-char keypad[16] = {'1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D'};
 
+int thresholds[16] = {209,152,86,9,373,336,296,250,481,456,429,398,559,541,521,499};
+char keypad[16] = {'1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D'};
 //char keypad[16] = {'1','2','3','A','4','5','6','B','7','8','9','C','E','0','F','D'};
 
 
@@ -17,25 +14,15 @@ void setup() {
 
 void loop() {
   CheckKeypad();
-  if(prevlen != buflen) {
-    prevlen++;
-    for (int i=0; i <= buflen; i++) {
-      Serial.write(buffer[i]);
-    }
-  }
 }
 
 bool CheckKeypad() { //char *buf, int index) 
   int value = analogRead(A0);
-
   for (int i=0; i < 16; i++)
   {
     if( abs(value - thresholds[i]) < 5)
     {
       Serial.println(keypad[i]);
-      buffer[buflen] = keypad[i];
-      buflen++;
-
       while (analogRead(A0) < 1000) {delay(100);}
     }
   }
